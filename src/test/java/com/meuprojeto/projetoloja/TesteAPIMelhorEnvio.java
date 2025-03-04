5e2dddb61c6918a170e3ae07ccfe6b6f53fbae74
Raphael Martinez <raphaelaml1@outlook.com>
2025-03-04 12:37:13 -0300

    Testes e criação para endpoint

--- src/test/java/com/meuprojeto/projetoloja/TesteAPIMelhorEnvio.java
+++ src/test/java/com/meuprojeto/projetoloja/TesteAPIMelhorEnvio.java
@@ -1,8 +1,6 @@
 package com.meuprojeto.projetoloja;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.meuprojeto.dto.EmpresaTransporteDTO;
+
 import com.meuprojeto.enums.ApiTokenIntegracao;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -10,9 +8,6 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 
 public class TesteAPIMelhorEnvio {
@@ -20,59 +15,62 @@ public class TesteAPIMelhorEnvio {
     public static void main(String[] args) throws Exception {
 
 
+        /**
+         * Faz a compra para etiqueta
+         */
+//        OkHttpClient client = new OkHttpClient();
+//        MediaType mediaType = MediaType.parse("application/json");
+//        RequestBody body = RequestBody.create(mediaType, "{\"orders\":[\"\"a54d16ed-1a73-4625-b73f-fb24833b9fb6\"]}");
+//        Request request = new Request.Builder()
+//                .url("https://sandbox.melhorenvio.com.br/api/v2/me/shipment/checkout")
+//                .post(body)
+//                .addHeader("Accept", "application/json")
+//                .addHeader("Content-Type", "application/json")
+//                .addHeader("Authorization", "Bearer " + ApiTokenIntegracao.TOKEN_MELHOR_ENVIO_SAND_BOX).build();
+//
+//        Response response = client.newCall(request).execute();
 
-        OkHttpClient client = new OkHttpClient().newBuilder() .build();
+
+
+
+
+//        OkHttpClient client = new OkHttpClient();
+//        MediaType mediaType = MediaType.parse("application/json");
+//        RequestBody body = RequestBody.create(mediaType, "{\"options\":{\"receipt\":true,\"own_hand\":true,\"reverse\":true,\"non_commercial\":true}}");
+//        Request request = new Request.Builder()
+//                .url(ApiTokenIntegracao.URL_MELHOR_ENVIO_SAND_BOX + "api/v2/me/cart")
+//                .post(body)
+//                .addHeader("Accept", "application/json")
+//                .addHeader("Content-Type", "application/json")
+//                .addHeader("Authorization", "Bearer " + ApiTokenIntegracao.TOKEN_MELHOR_ENVIO_SAND_BOX)
+//                .addHeader("User-Agent", "suporte@jdevtreinamento.com.br")
+//                .build();
+//
+//        Response response = client.newCall(request).execute();
+//
+//        System.out.println(response.body().string());
+
+
+        /**
+         * Geração de etiquetas
+         */
+        OkHttpClient client = new OkHttpClient();
+
         MediaType mediaType = MediaType.parse("application/json");
-        RequestBody body = RequestBody.create(mediaType, "{ \"from\": { \"postal_code\": \"96020360\" }, \"to\": { \"postal_code\": \"01018020\" }, \"products\": [ { \"id\": \"x\", \"width\": 11, \"height\": 17, \"length\": 11, \"weight\": 0.3, \"insurance_value\": 10.1, \"quantity\": 1 }, { \"id\": \"y\", \"width\": 16, \"height\": 25, \"length\": 11, \"weight\": 0.3, \"insurance_value\": 55.05, \"quantity\": 2 }, { \"id\": \"z\", \"width\": 22, \"height\": 30, \"length\": 11, \"weight\": 1, \"insurance_value\": 30, \"quantity\": 1 } ] }");
+        RequestBody body = RequestBody.create(mediaType, "{\"orders\":[\"string\"]}");
         Request request = new Request.Builder()
-                .url(ApiTokenIntegracao.URL_MELHOR_ENVIO_SAND_BOX +"api/v2/me/shipment/calculate")
-                .method("POST", body)
+                .url("https://sandbox.melhorenvio.com.br/api/v2/me/shipment/generate")
+                .post(body)
                 .addHeader("Accept", "application/json")
                 .addHeader("Content-Type", "application/json")
-                .addHeader("Authorization", "Bearer " + ApiTokenIntegracao.TOKEN_MELHOR_ENVIO_SAND_BOX)
-                .addHeader("User-Agent", "suporte@jdevtreinamento.com.br")
+                .addHeader("Authorization", "Bearer token")
+                .addHeader("User-Agent", "Aplicação (email para contato técnico)")
                 .build();
 
         Response response = client.newCall(request).execute();
-        //System.out.println(response.body().string());
 
-        JsonNode jsonNode = new ObjectMapper().readTree(response.body().string());
-
-        Iterator<JsonNode> iterator = jsonNode.iterator();
-
-        List<EmpresaTransporteDTO> empresaTransporteDTOs = new ArrayList<EmpresaTransporteDTO>();
-
-        while(iterator.hasNext()) {
-            JsonNode node = iterator.next();
-
-            EmpresaTransporteDTO empresaTransporteDTO = new EmpresaTransporteDTO();
-
-            if (node.get("id") != null) {
-                empresaTransporteDTO.setId(node.get("id").asText());
-            }
-
-            if (node.get("name") != null) {
-                empresaTransporteDTO.setNome(node.get("name").asText());
-            }
-
-            if (node.get("price") != null) {
-                empresaTransporteDTO.setValor(node.get("price").asText());
-            }
-
-            if (node.get("company") != null) {
-                empresaTransporteDTO.setEmpresa(node.get("company").get("name").asText());
-                empresaTransporteDTO.setPicture(node.get("company").get("picture").asText());
-            }
-
-            if (empresaTransporteDTO.dadosOK()) {
-                empresaTransporteDTOs.add(empresaTransporteDTO);
-            }
-        }
-
-        System.out.println(empresaTransporteDTOs);
 
 
     }
-    
 
 }
