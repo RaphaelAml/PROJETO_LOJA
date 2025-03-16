1a4fffcd1d4b9beadb7e964ce263a4723f8c7eda
Raphael Martinez <raphaelaml1@outlook.com>
2025-03-16 10:11:17 -0300

    Service SSL

--- src/main/java/com/meuprojeto/controller/Vd_Cp_loja_Virt_Controller.java
+++ src/main/java/com/meuprojeto/controller/Vd_Cp_loja_Virt_Controller.java
@@ -13,6 +13,7 @@ import com.meuprojeto.service.VendaService;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -98,16 +99,6 @@ public class Vd_Cp_loja_Virt_Controller {
 
         /* Salva primeiro a venda e todo os dados */
         vendaCompraLojaVirtual = vd_Cp_Loja_virt_repository.saveAndFlush(vendaCompraLojaVirtual);
-
-        StatusRastreio statusRastreio = new StatusRastreio();
-        statusRastreio.setCentroDistribuicao("Loja Local");
-        statusRastreio.setCidade("Local");
-        statusRastreio.setEmpresa(vendaCompraLojaVirtual.getEmpresa());
-        statusRastreio.setEstado("Local");
-        statusRastreio.setStatus("Inicio Compra");
-        statusRastreio.setVendaCompraLojaVirtual(vendaCompraLojaVirtual);
-
-        statusRastreioRepository.save(statusRastreio);
 
 
         /* Associa a venda gravada no banco com a nota fiscal */
@@ -626,6 +617,52 @@ public class Vd_Cp_loja_Virt_Controller {
 
         jdbcTemplate.execute("begin; update vd_cp_loja_virt_set url_imprimi_etiqueta = '"+urlEtiqueta+"' where id = " + compraLojaVirtual.getId() + ";commit;");
         //vd_Cp_Loja_virt_repository.updateURLEtiqueta(urlEtiqueta, compraLojaVirtual.getId());
+
+        OkHttpClient clientRastreio = new OkHttpClient().newBuilder().build();
+        okhttp3.MediaType mediaTypeR = okhttp3.MediaType.parse("application/json");
+        okhttp3.RequestBody bodyR = okhttp3.RequestBody.create(mediaTypeR, "{\n    \"orders\": [\n        \"a54d16ed-1a73-4625-b73f-fb24833b9fb6\"\n    ]\n}");
+        okhttp3.Request requestR = new Request.Builder()
+                .url(ApiTokenIntegracao.URL_MELHOR_ENVIO_SAND_BOX+ "api/v2/me/shipment/tracking")
+                .method("POST", bodyR)
+                .addHeader("Accept", "application/json")
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", "Bearer " +  ApiTokenIntegracao.TOKEN_MELHOR_ENVIO_SAND_BOX)
+                .addHeader("User-Agent", "suporte@jdevtreinamento.com.br").build();
+
+        Response responseR = clientRastreio.newCall(requestR).execute();
+
+
+        JsonNode jsonNodeR = new ObjectMapper().readTree(responseR.body().string());
+
+
+        Iterator<JsonNode> iteratorR = jsonNodeR.iterator();
+
+        String idEtiquetaR = "";
+
+        while(iteratorR.hasNext()) {
+            JsonNode node = iteratorR.next();
+            if (node.get("tracking") != null) {
+                idEtiquetaR = node.get("tracking").asText();
+            }else {
+                idEtiquetaR = node.asText();
+            }
+            break;
+        }
+
+        List<StatusRastreio> rastreios =	statusRastreioRepository.listaRastreioVenda(idVenda);
+
+        if (rastreios.isEmpty()) {
+
+            StatusRastreio rastreio = new StatusRastreio();
+            rastreio.setEmpresa(compraLojaVirtual.getEmpresa());
+            rastreio.setVendaCompraLojaVirtual(compraLojaVirtual);
+            rastreio.setUrlRastreio("https://www.melhorrastreio.com.br/rastreio/" + idEtiquetaR);
+
+            statusRastreioRepository.saveAndFlush(rastreio);
+        }else {
+            statusRastreioRepository.salvaUrlRastreio("https://www.melhorrastreio.com.br/rastreio/" + idEtiquetaR, idVenda);
+        }
+
 
         return new ResponseEntity<String>("Sucesso", HttpStatus.OK);
 
--- src/main/java/com/meuprojeto/repository/StatusRastreioRepository.java
+++ src/main/java/com/meuprojeto/repository/StatusRastreioRepository.java
@@ -2,6 +2,7 @@ package com.meuprojeto.repository;
 
 import com.meuprojeto.model.StatusRastreio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -12,6 +13,11 @@ public interface StatusRastreioRepository extends JpaRepository<StatusRastreio, 
     @Query(value = "select s from StatusRastreio  s where s.vendaCompraLojaVirtual.id = ?1 order by s.id")
     public List<StatusRastreio> listaRastreioVenda(Long id);
 
+
+
+    @Modifying(flushAutomatically = true)
+    @Query(nativeQuery = true, value = "update vd_cp_loja_virt set url_rastreio = ?1 where id = ?2")
+    public void salvaUrlRastreio(String urlRastreio, Long idVenda);
 
 
 }
